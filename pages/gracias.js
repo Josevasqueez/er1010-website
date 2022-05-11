df5d4e524dd7ecc254afda036504c452f9509ab4
Alejandro <alejandrovasqueez@gmail.com>
2022-05-11 10:15:20 -0400

    Add conversion

--- pages/gracias.js
+++ pages/gracias.js
@@ -10,7 +10,7 @@ import Button from '../components/blocks/Button'
 export default function thankYou({ content, locale }) {
 
     const seo = () => {
-        if(locale.includes('en')) return { metaTitle: "Thank you for contacting us", metaDescription: "You have contacted us, we are the Ezequiel Reyna Law Office, experienced personal injury attorneys" }
+        if (locale.includes('en')) return { metaTitle: "Thank you for contacting us", metaDescription: "You have contacted us, we are the Ezequiel Reyna Law Office, experienced personal injury attorneys" }
         return { metaTitle: "Gracias por contactarnos", metaDescription: "Nos ha contactado, somos la oficina legal de Ezequiel Reyna, abogados con experiencia en lesiones personales." }
     }
 
@@ -23,6 +23,10 @@ export default function thankYou({ content, locale }) {
                     <br />
                     <Link href={content.button.url}><a><Button>{content.button.text}</Button></a></Link>
                 </div>
+                <script dangerouslySetInnerHTML={{
+                    __html: `gtag('event', 'conversion', {'send_to': 'AW-10843878606/zh2NCP_ipLADEM7p4bIo'});`
+                    }}>
+                </script>
             </Section>
         </Layout>
     )
