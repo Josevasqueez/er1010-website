91ba6e7b873c992614759f9cc90535e67afa4bad
Alejandro <alejandrovasqueez@gmail.com>
2022-05-31 14:42:41 -0400

    fix

--- components/Layout.js
+++ components/Layout.js
@@ -62,11 +62,12 @@ export default function Layout({ children, seo, navigation, footer, dark = false
                         __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PNFD3JB"
                         height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}>
                     </noscript>
-                    <Script dangerouslySetInnerHTML={{
+                    <script dangerouslySetInnerHTML={{
                         __html: `var script = document.createElement('script');
                         script.async = true; script.type = 'text/javascript';
                         var target = 'https://www.clickcease.com/monitor/stat.js';
-                        script.src = target;var elem = document.head;elem.appendChild(script);`}} />
+                        script.src = target;var elem = document.head;elem.appendChild(script);`}}>
+                    </script>
                     <noscript dangerouslySetInnerHTML={{
                         __html: `<a href='https://www.clickcease.com' rel='nofollow'><img src='https://monitor.clickcease.com/stats/stats.aspx' alt='ClickCease'/></a>`
                     }}>
