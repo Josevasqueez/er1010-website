afb607d92aad372cabbf369d6a3834dbce63d2bf
Alejandro <alejandrovasqueez@gmail.com>
2022-05-31 14:39:03 -0400

    lazy scripts

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 import Footer from "./Footer";
 import Navigation from "./Navigation";
 
@@ -19,14 +20,13 @@ export default function Layout({ children, seo, navigation, footer, dark = false
                     gtag('config', 'AW-10843878606');
                     gtag('config', 'UA-216071602-1');`}}>
                 </script> */}
-                <script dangerouslySetInnerHTML={{
+                <Script dangerouslySetInnerHTML={{
                     __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','GTM-PNFD3JB');`}}>
-                </script>
-                <script dangerouslySetInnerHTML={{
+                    })(window,document,'script','dataLayer','GTM-PNFD3JB');`}} />
+                <Script dangerouslySetInnerHTML={{
                     __html: `!function(f,b,e,v,n,t,s)
                     {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
                     n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -36,8 +36,7 @@ export default function Layout({ children, seo, navigation, footer, dark = false
                     s.parentNode.insertBefore(t,s)}(window, document,'script',
                     'https://connect.facebook.net/en_US/fbevents.js');
                     fbq('init', '629334045055055');
-                    fbq('track', 'PageView');`}}>
-                </script>
+                    fbq('track', 'PageView');`}} />
                 <noscript dangerouslySetInnerHTML={{
                     __html: `<img height="1" width="1" style="display:none"
                     src="https://www.facebook.com/tr?id=629334045055055&ev=PageView&noscript=1"
@@ -63,19 +62,18 @@ export default function Layout({ children, seo, navigation, footer, dark = false
                         __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PNFD3JB"
                         height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}>
                     </noscript>
-                    <script dangerouslySetInnerHTML={{
+                    <Script dangerouslySetInnerHTML={{
                         __html: `var script = document.createElement('script');
                         script.async = true; script.type = 'text/javascript';
                         var target = 'https://www.clickcease.com/monitor/stat.js';
-                        script.src = target;var elem = document.head;elem.appendChild(script);`}}>
-                    </script>
+                        script.src = target;var elem = document.head;elem.appendChild(script);`}} />
                     <noscript dangerouslySetInnerHTML={{
                         __html: `<a href='https://www.clickcease.com' rel='nofollow'><img src='https://monitor.clickcease.com/stats/stats.aspx' alt='ClickCease'/></a>`
-                        }}>
+                    }}>
                     </noscript>
                     <Footer content={footer} />
                 </>
             }
-        </div>
+        </div >
     )
 }
