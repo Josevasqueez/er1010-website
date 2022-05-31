160548ae89d0925cd40e434813784494f8733b4b
Alejandro <alejandrovasqueez@gmail.com>
2022-05-31 14:45:00 -0400

    add ID

--- components/Layout.js
+++ components/Layout.js
@@ -20,13 +20,13 @@ export default function Layout({ children, seo, navigation, footer, dark = false
                     gtag('config', 'AW-10843878606');
                     gtag('config', 'UA-216071602-1');`}}>
                 </script> */}
-                <Script dangerouslySetInnerHTML={{
+                <Script id="google-tag-manager" dangerouslySetInnerHTML={{
                     __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                     })(window,document,'script','dataLayer','GTM-PNFD3JB');`}} />
-                <Script dangerouslySetInnerHTML={{
+                <Script id="facebook-pixel" dangerouslySetInnerHTML={{
                     __html: `!function(f,b,e,v,n,t,s)
                     {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
                     n.callMethod.apply(n,arguments):n.queue.push(arguments)};
